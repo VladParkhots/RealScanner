5f70c0662ff2004fe84da10b85b1b50b2c8285b4
VladParkhots <80053156+VladParkhots@users.noreply.github.com>
2021-03-05 16:48:34 +0300

    exercise 4 - 4

---
+++ src/com/lesson4/Exercise4_4.java
@@ -0,0 +1,44 @@
+package com.lesson4;
+
+import java.util.Random;
+
+public class Exercise4_4 {
+    public static void main(String [] args){
+        Random rand = new Random();
+
+        int bestResult = 0;
+        int index = 0;
+
+
+        int [] mas = new int [100];
+
+        for (int i= 0; i < mas.length; i++) {
+            mas[i] = rand.nextInt(10000);
+        }
+
+        for (int i= 0; i < mas.length; i++){
+
+            int x = i-1;
+            int y = i+1;
+
+            if (i == 0){
+                continue;
+            }
+            if (i == mas.length-1){
+                continue;
+            }
+            System.out.print(mas[x]+" "+mas[i] + " "+ mas[y]);
+
+            int resultOf3 = mas[x] + mas[i] + mas[y];
+            System.out.println(" = "+ resultOf3);
+            if (resultOf3 > bestResult){
+                bestResult = resultOf3;
+                index = x;
+            }
+        }
+        System.out.println(bestResult);
+        System.out.println(index);
+
+
+    }
+}
--- src/com/lesson4/wdawdaw.java
+++
@@ -1,7 +0,0 @@
-package com.lesson4;
-
-public class wdawdaw {
-    public static void main(String [] args){
-        System.out.println("Hello GIT!");
-    }
-}
